06b293622258382ce5159ca7eff8c68c3571e6e4
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2021-10-12 00:41:49 +0100

    clean utils

--- src/main/java/op/congreso/pleno/DownloadPdfs.java
+++ src/main/java/op/congreso/pleno/DownloadPdfs.java
@@ -3,11 +3,7 @@ package op.congreso.pleno;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -20,7 +16,6 @@ public class DownloadPdfs {
         var plenos = jsonMapper.readValue(bytes,  new TypeReference<List<Pleno>>() {});
 
         plenos.stream()
-                .filter(pleno -> pleno.paginas() < 1)
                 .map(Pleno::directory)
                 .collect(Collectors.toSet())
                 .forEach(s -> {
@@ -32,21 +27,9 @@ public class DownloadPdfs {
                 });
 
         plenos.stream()
-                .filter(pleno -> pleno.paginas() < 1)
                 .parallel()
-                .forEach(s -> {
-                    download(s);
-                });
+                .forEach(Pleno::download);
     }
 
-    private static void download(Pleno s) {
-        try {
-            ReadableByteChannel readableByteChannel = Channels.newChannel(new URL(s.url()).openStream());
-            FileOutputStream fileOutputStream = new FileOutputStream("out/" + s.path());
-            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
